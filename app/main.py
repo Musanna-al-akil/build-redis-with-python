46c3619e76c3fcf1e14e2294c406d452c34f16e6
Musanna Al Akil <musannaal@gmail.com>
2024-02-27 22:32:04 +0600

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -6,10 +6,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    pong = "+PONG\r\n"
+
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()
+    
+    #the with statement used for continuse use case
+    with conn:
+        conn.recv(1024)
+        conn.send(pong.encode())
 
 
 if __name__ == "__main__":
